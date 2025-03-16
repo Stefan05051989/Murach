ae8969f30c26ba0e18b657d6d696086e7c31f55a
Stefan <stefan.kiers89@hotmail.com>
2025-03-16 16:35:18 +0100

    class and statement build up

---
+++ src/main/java/Syntax/ClassName.java
@@ -0,0 +1,17 @@
+package Syntax;
+/*
+S. Kiers
+16-03
+
+basic stuff to add at git
+ */
+
+
+// Package : ClasName
+//
+public class ClassName {
+    // Main Method declared within the curley braces : PSVM
+    public static void main(String[] args) {
+        // here is room for statements within the main method.
+    }
+}
